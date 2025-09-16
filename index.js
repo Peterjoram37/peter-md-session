48ae998804697ea8e3da47fe25f6e14f4ffa0718
Peterjoram37 <videohidden21@gmail.com>
2025-09-16 16:17:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
 const pairRouter = require('./pair');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Routes
-app.use('/paircode', pairRouter); // backend for sessionId
-app.get('/pair', (req,res)=> res.sendFile(path.join(__dirname,'pair.html')));
-app.get('/', (req,res)=> res.sendFile(path.join(__dirname,'main.html')));
+app.use(express.static(__dirname));
+app.use('/pair', pairRouter);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pair.html'));
 });
 
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
